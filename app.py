a568f674c89af335882c869e23c3946a471b7d2b
Claire Burghard <claireburghard@gmail.com>
2015-04-30 06:08:12 -0400

    worked on html files

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ app.secret_key = 'secret'
 @app.route("/", methods=['GET','POST'])
 @app.route("/index", methods=['GET','POST'])
 def index():
-    message = "this is a message"
+    message = ""
     if request.method=="GET":
         return render_template("index.html", message=message)
     else:
         return render_template("index.html", message=message)
 
+@app.route("/signup",methods=['GET','POST'])
+def signup():
+    message = ""
+    if request.method=="GET":
+        return render_template("signup.html", message=message)
+    else:
+        return render_template("signup.html", message=message)
+    
 if __name__=="__main__":
     app.debug=True
     app.run()
