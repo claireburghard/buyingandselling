23bd015b56720d581e1830050e35dd39d3a52676
Claire Burghard <claireburghard@gmail.com>
2015-04-30 12:32:46 -0400

    login system workinggit add --all

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, flash,  render_template, request, redirect, url_for, session, escape
 
+import mongo
+
 
 app = Flask(__name__)
 app.secret_key = 'secret'
@@ -7,11 +9,44 @@ app.secret_key = 'secret'
 @app.route("/", methods=['GET','POST'])
 @app.route("/index", methods=['GET','POST'])
 def index():
-    message = ""
-    if request.method=="GET":
-        return render_template("index.html", message=message)
+    if 'username' in session:
+        return redirect(url_for('home'))
     else:
-        return render_template("index.html", message=message)
+        message = ""
+        if request.method=="GET":
+            return render_template("index.html", message=message)
+        else:
+            if request.form['b']=="Log In":
+                username = request.form["logusername"]
+                password = request.form["logpassword"]
+                confirmation = mongo.authenticate(username,password)
+                if confirmation != "match":
+                    message = confirmation
+                    return render_template("index.html", message=message)
+                if confirmation == "match":
+                    session['username'] = username
+                    return redirect(url_for('home'))
+            if request.form['b']=="Sign Up":
+                username = request.form["signusername"]
+                password = request.form["signpassword"]
+                password2 = request.form["signpassword2"]
+                name = request.form["name"]
+                if mongo.user_exists(username) == "exists":
+                    message = "Someone already has this username. Please use a different one." 
+                    return render_template("index.html", message=message)
+                else:
+                    if password == password2:
+                        mongo.add_user(username,password,name)
+                        message = "Registration Sucessful! Log In to get started." 
+                        return render_template("index.html", message=message)
+                    else:
+                        message = "Please make sure your passwords match"
+                        return render_template("index.html", message=message)
+            if request.form['b']=="Cancel":
+                return render_template("index.html", message=message)
+
+
+
 
 @app.route("/signup",methods=['GET','POST'])
 def signup():
@@ -20,6 +55,10 @@ def signup():
         return render_template("signup.html", message=message)
     else:
         return render_template("signup.html", message=message)
+
+@app.route("/home",methods=['GET','POST'])
+def home():
+    return render_template('home.html')
     
 if __name__=="__main__":
     app.debug=True
