1aaa2ab6e8d822308f66c0f0fd47146995a9b73c
Claire Burghard <claireburghard@gmail.com>
2015-05-11 12:31:50 -0400

    formatted html

--- app.py
+++ app.py
@@ -64,15 +64,10 @@ def home():
         else:
             print "logout"
             if request.form['b']=="Logout":
-                #print "logout"
+                print "logout"
                 return redirect(url_for('logout'))
             if request.form['b']=="Submit":
-                title = request.form['title']
-                content = request.form['content']
-                price = request.form['price']
-                # don't know/ trying to figure out how to get the username from the session-- if you can help please do
-                #mongo.add_post('username', title, content, price)
-                
+                pass
     else:
         return redirect(url_for('index'))
 
@@ -84,6 +79,47 @@ def profile():
             return render_template('profile.html')
         else:
             return render_template('profile.html')
+    else:
+        return redirect(url_for('index'))
+
+@app.route("/market",methods=['GET','POST'])
+def market():
+    if 'username' in session:
+        if request.method=="GET":
+            return render_template('market.html')
+        else:
+            return render_template('market.html')
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route("/myitems",methods=['GET','POST'])
+def myitems():
+    if 'username' in session:
+        if request.method=="GET":
+            return render_template('myitems.html')
+        else:
+            return render_template('myitems.html')
+
+@app.route("/myactivity",methods=['GET','POST'])
+def myactivity():
+    if 'username' in session:
+        if request.method=="GET":
+            return render_template('myactivity.html')
+        else:
+            return render_template('myactivity.html')
+    else:
+        return redirect(url_for('index'))
+
+@app.route("/messages",methods=['GET','POST'])
+def messages():
+    if 'username' in session:
+        if request.method=="GET":
+            return render_template('messages.html')
+        else:
+            return render_template('messages.html')
+    else:
+        return redirect(url_for('index'))
 
     
 @app.route("/logout")
